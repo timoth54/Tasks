51f754c8f731beb45851a46fa016795c077aba61
timoth54 <timoth54@ksu.edu>
2019-11-01 08:16:06 -0500

    Initial Commit

---
+++ TaskViewer/TaskViewer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskViewer
+{
+    class TaskViewer
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        internal static void chkPalindrome(string str)
+        {
+
+        }
+
+        internal static void FindSecondLargeInArray(int[] arr)
+        {
+
+        }
+
+        internal static bool FindPrime(int number)
+        {
+
+        }
+    }
+}
